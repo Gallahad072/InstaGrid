56f0a7d93bf77a484d40e57446f0c9ba85add21d
Gallahad072 <gallahad072@gmail.com>
2023-03-30 17:39:06 +0100

    style: pep-8

--- main.py
+++ main.py
@@ -1,42 +1,40 @@
 from datetime import datetime
 import numpy as np
 import os
-from os import path
 import shutil
 
 import instaloader
 import cv2
 
+ASPECTS = {8: (4, 2), 18: (6, 3), 32: (8, 4), 10: (2, 5), 21: (3, 7)}
+
 
 class InstaCollage:
     def __init__(self, username) -> None:
-        self.L = instaloader.Instaloader()
-        self.L.interactive_login(username)
+        self.loader = instaloader.Instaloader()
+        self.loader.interactive_login(username)
         print("Login Successful")
 
     def saveCollage(self):
-        aspects = {8: (4, 2), 18: (6, 3), 32: (8, 4), 10: (2, 5), 21: (3, 7)}
-
         try:
-            picsInDir = len(os.listdir("pics"))
-            numOfRows = aspects[picsInDir][1]
+            dir_pics = len(os.listdir("pics"))
+            num_of_rows = ASPECTS[dir_pics][1]
         except (FileNotFoundError, KeyError) as e:
             print("Error: Run getPics()")
             return False
 
-        openImgs = [cv2.imread(f"pics/{i+1}.jpg") for i in range(picsInDir)]
-
+        open_pics = [cv2.imread(f"pics/{i+1}.jpg") for i in range(dir_pics)]
         arrays = []
-        for x in openImgs:
-            axisLen = min(x.shape[:2])
-            rowStart = x.shape[0] // 2 - axisLen // 2
-            rowEnd = rowStart + axisLen
-            colStart = x.shape[1] // 2 - axisLen // 2
-            colEnd = colStart + axisLen
-            arrays.append(x[rowStart:rowEnd, colStart:colEnd])
+        for x in open_pics:
+            axis_len = min(x.shape[:2])
+            row_start = x.shape[0] // 2 - axis_len // 2
+            row_end = row_start + axis_len
+            col_start = x.shape[1] // 2 - axis_len // 2
+            col_end = col_start + axis_len
+            arrays.append(x[row_start:row_end, col_start:col_end])
 
         arrays = [cv2.resize(x, (720, 720)) for x in arrays]
-        rows = [np.hstack(x) for x in np.array_split(arrays, numOfRows)]
+        rows = [np.hstack(x) for x in np.array_split(arrays, num_of_rows)]
         self.collage = np.vstack(rows)
         cv2.imwrite("collage.jpg", self.collage)
         print("\nSuccess: Collage Saved\n")
@@ -44,22 +42,21 @@ class InstaCollage:
 
     def getPics(self, search, landscape=True):
         totals = [8, 18, 32] if landscape else [10, 21]
-        self.prof = instaloader.Profile.from_username(self.L.context, search)
+        self.prof = instaloader.Profile.from_username(self.loader.context, search)
         try:
-            numOfPics = max(x for x in totals if x <= self.prof.mediacount)
+            num_of_pics = max(x for x in totals if x <= self.prof.mediacount)
         except ValueError:
             print(f"Error: Minimum {min(totals)} pics for this orientation")
             return False
 
-        if path.exists("pics"):
+        if os.path.exists("pics"):
             shutil.rmtree("pics")
         os.mkdir("pics")
-        print(f"Downloading {numOfPics} pics...")
+        print(f"Downloading {num_of_pics} pics...")
         for i, post in enumerate(self.prof.get_posts()):
-            if i == numOfPics:
+            if i == num_of_pics:
                 break
-            self.L.download_pic(f"pics/{str(i+1)}", post.url, datetime.now())
-
+            self.loader.download_pic(f"pics/{str(i+1)}", post.url, datetime.now())
         return True
 
     def run(self, search, landscape=True):
@@ -69,5 +66,5 @@ class InstaCollage:
 
 
 if __name__ == "__main__":
-    ig = InstaCollage("rhys.zip")
+    ig = InstaCollage("rhyswalexander")
     ig.run("openaidalle")
